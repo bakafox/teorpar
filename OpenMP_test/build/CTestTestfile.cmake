a99776482230941ff997259af7d38b9523ccde99
bakafox <v.chekhovskii@g.nsu.ru>
2024-02-28 08:58:13 +0700

    Добавлены .pdf-версии документов

--- OpenMP_test/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/fisk/Рабочий стол/teorpar/Задание 2
-# Build directory: /home/fisk/Рабочий стол/teorpar/Задание 2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
